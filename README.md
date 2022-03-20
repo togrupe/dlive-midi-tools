e79b6c6c20bc5a89def69dade4c902cb7f8eb4c9
Tobias Grupe <tobias.grupe@gmx.de>
2022-03-21 00:23:14 +0100

    Documentation improved Comments added for dliveConstants

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ The following colors are allowed:
 blue, red, light blue, magenta, green, yellow, black, white
 
 The Midi Channel setting on dlive under Utils/Shows -> Control -> Midi has to be: 1 to 5
+
+The default mixrack ip address is: 192.168.1.70. This ip is preconfigured as well the the scripts. If you want to change it, you can edit the field "ip" in the file: dliveConstants.py.   
--- dliveConstants.py
+++ dliveConstants.py
@@ -1,3 +1,4 @@
+# The color mappings
 lcd_color_black = 0x00
 lcd_color_red = 0x01
 lcd_color_green = 0x02
@@ -7,11 +8,13 @@ lcd_color_purple = 0x05
 lcd_color_ltblue = 0x06
 lcd_color_white = 0x07
 
+# The phantom power values
 phantom_power_off = 0x00
 phantom_power_on = 0x7F
 
 sysexhdrstart = [0xF0, 0x00, 0x00, 0x1A, 0x50, 0x10, 0x01, 0x00]
 sysexhdrend = [0xF7]
 
+# mixrack ip and port for midi/tcp
 ip = '192.168.1.70'
 port = 51325

ca43bc0ade70a1941c32d3f317369befc72528a0
Tobias Grupe <tobias.grupe@gmx.de>
2022-10-19 03:26:59 +0200

    First running version of Progressbar

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 import re
 import time
 from tkinter import filedialog, Button, Tk, Checkbutton, IntVar, W, Frame, LEFT, YES, TOP, X, GROOVE, RIGHT, Label, \
-    Entry, BOTTOM, StringVar, OptionMenu
+    Entry, BOTTOM, StringVar, OptionMenu, ttk
+from tkinter.messagebox import showinfo
 
 import mido
 import pandas as pd
@@ -11,7 +12,7 @@ from mido.sockets import connect
 import dliveConstants
 from ChannelListEntry import ChannelListEntry
 
-version = "1.4.0"
+version = "1.5.0"
 
 is_network_communication_allowed = dliveConstants.allow_network_communication
 
@@ -29,7 +30,6 @@ def trigger_channel_renaming(message, output, names):
         else:
             trimmed_name = str(item.get_name())
 
-        # print("processing channel name: " + str(trimmed_name))
         characters = re.findall('.?', trimmed_name)
 
         payload = []
@@ -150,6 +150,7 @@ def read_document(filename, check_box_states):
         print("Connection successful.")
     else:
         output = None
+    progress_open_or_close_connection()
 
     root.midi_port = determine_technical_midi_port(var_midi_port.get())
 
@@ -157,25 +158,52 @@ def read_document(filename, check_box_states):
 
     print("Start Processing...")
 
+    actions = 0
+
     if check_box_states.__getitem__(0):  # Names
+        actions = actions + 1
+        naming = True
+    else:
+        naming = False
+
+    if check_box_states.__getitem__(1):  # Colors
+        actions = actions + 1
+        coloring = True
+    else:
+        coloring = False
+
+    if check_box_states.__getitem__(2):  # Phantom power
+        actions = actions + 1
+        phantoming = True
+    else:
+        phantoming = False
+
+    if naming:
         print("Writing the following channel names...")
         print("Input Array: " + str(names))
         trigger_channel_renaming("Naming the channels...", output, channel_list_entries)
+        progress(actions)
 
-    if check_box_states.__getitem__(1):  # Colors
+    if coloring:
         print("Writing the following colors...")
         print("Input Array: " + str(colors))
         trigger_coloring("Coloring the channels...", output, channel_list_entries)
+        progress(actions)
 
-    if check_box_states.__getitem__(2):  # Phantom power
+    if phantoming:
         print("Writing the following phantom power values...")
         print("Input Array: " + str(phantoms))
         trigger_phantom_power("Set phantom power to the channels...", output, channel_list_entries)
+        progress(actions)
+
+    if actions == 0:
+        progress(actions)
 
     print("Processing done")
 
     if is_network_communication_allowed:
         output.close()
+    progress_open_or_close_connection()
 
 
 def determine_technical_midi_port(selected_midi_port_as_string):
@@ -240,7 +268,7 @@ def get_checkbox_states():
 
 if __name__ == '__main__':
     root.title('Channel List Manager for Allen & Heath dLive Systems - v' + version)
-    root.geometry('600x250')
+    root.geometry('600x300')
     root.resizable(False, False)
     Label(root, text=" ").pack(side=TOP)
     Label(root, text="Choose from the given Excel sheet which column you want to write.").pack(side=TOP)
@@ -278,7 +306,45 @@ if __name__ == '__main__':
 
     Button(bottom_frame, text='Open Excel sheet and trigger writing process', command=browse_files).grid(row=0)
     Label(bottom_frame, text=" ", width=30).grid(row=1)
-    Button(bottom_frame, text='Quit', command=root.quit).grid(row=2)
+
+    pb = ttk.Progressbar(
+        bottom_frame,
+        orient='horizontal',
+        mode='determinate',
+        length=600
+    )
+
+    pb.grid(row=2)
+
+
+    def update_progress_label():
+        return f"Current Progress: {pb['value']}%"
+
+
+    def progress(actions=None):
+        if actions == 0:
+            pb['value'] += 90
+        else:
+            if pb['value'] < 100:
+                pb['value'] += 90 / actions
+                value_label['text'] = update_progress_label()
+            else:
+                showinfo(message='The progress completed!')
+
+
+    def progress_open_or_close_connection():
+        if pb['value'] < 100:
+            pb['value'] += 5
+            value_label['text'] = update_progress_label()
+        else:
+            showinfo(message='The progress completed!')
+
+
+    # label to show current value in percent
+    value_label = ttk.Label(bottom_frame, text=update_progress_label())
+    value_label.grid(row=3)
+
+    Button(bottom_frame, text='Quit', command=root.quit).grid(row=4)
     bottom_frame.pack(side=BOTTOM)
 
     root.mainloop()

53ba695d58a80d7b9002d3cdf5e50478ef090ed8
Tobias Grupe <tobias.grupe@gmx.de>
2023-01-06 02:41:51 +0100

    Master Out Patching fixed

--- dawsession/SessionCreator.py
+++ dawsession/SessionCreator.py
@@ -44,6 +44,10 @@ def generate_hwout_item(channel):
 def create_reaper_session(sheet):
     project = Project()
 
+    project.props = [
+        ["MASTERHWOUT", ""]
+    ]
+
     for item in sheet.get_channel_model():
         track = Track()
         name = item.get_name()
@@ -59,7 +63,7 @@ def create_reaper_session(sheet):
             ["PEAKCOL", convert_sheet_color_to_reaper_color(item.get_color())],
             ["REC", generate_rec_item(item.get_channel_dlive())],
             ["TRACKHEIGHT", "40 0 0 0 0 0"],
-            ["HWOUT", generate_hwout_item(item.get_channel_dlive())]
+            ["HWOUT", generate_hwout_item(item.get_channel_dlive())],
         ]
         project.add(track)
 

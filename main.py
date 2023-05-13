d5248f5a0f18a27d1986b7389b77a8e5c71ba73c
Tobias Grupe <tobias.grupe@gmx.de>
2023-05-13 17:19:48 +0200

    sleeptime a little bit increased testsheet adopted

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ from model.Sheet import Sheet
 LABEL_IPADDRESS_AVANTIS = "IP-Address:"
 LABEL_IPADDRESS_DLIVE = "Mixrack IP-Address:"
 
-DEFAULT_SLEEP_AFTER_MIDI_COMMAND = 0.001
+DEFAULT_SLEEP_AFTER_MIDI_COMMAND = 0.005
 
 logging.basicConfig(filename='main.log', level=logging.DEBUG)
 

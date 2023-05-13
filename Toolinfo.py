cb6480315cb8cb01af70452bb93646a4bf69768e
Tobias Grupe <tobias.grupe@gmx.de>
2023-05-13 17:22:53 +0200

    sleeptime a little bit increased testsheet adopted

--- Toolinfo.py
+++ Toolinfo.py
@@ -5,5 +5,5 @@
 #
 ####################################################
 tool_name = "Channel List Manager for Allen & Heath dLive and Avantis"
-version = "2.3.0-RC4"
+version = "2.3.0-RC2"
 build_date = "May 2023"

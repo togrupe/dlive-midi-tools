21c73685735ec020f8ea3278df92d6c8a94f1c07
Tobias Grupe <tobias.grupe@gmx.de>
2023-08-01 16:17:28 +0200

    version updated

--- src/Toolinfo.py
+++ src/Toolinfo.py
@@ -6,5 +6,5 @@
 #
 ####################################################
 tool_name = "Channel List Manager for Allen & Heath dLive and Avantis"
-version = "2.4.1-RC1"
+version = "2.4.1-RC2"
 build_date = "July 2023"

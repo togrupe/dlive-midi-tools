9996ff1774d6aee6ef713110c9c24a1a4b042d2a
Tobias Grupe <tobias.grupe@gmx.de>
2026-05-09 22:18:30 +0200

    files added, version updated

---
+++ src/AppData.py
@@ -0,0 +1,51 @@
+class AppData:
+    def __init__(self, midi_channel, console, current_ip):
+        self.midi_channel = midi_channel
+        self.console = console
+        self.current_ip = current_ip
+        self.output_reaper = False
+        self.output_trackslive = False
+        self.output_write_to_console = False
+        self.output_write_csv = False
+
+    def get_midi_channel(self):
+        return self.midi_channel
+
+    def set_midi_channel(self, midi_channel):
+        self.midi_channel = midi_channel
+
+    def get_console(self):
+        return self.console
+
+    def set_console(self, console):
+        self.console = console
+
+    def get_current_ip(self):
+        return self.current_ip
+
+    def set_current_ip(self, current_ip):
+        self.current_ip = current_ip
+
+    def get_output_reaper(self):
+        return self.output_reaper
+
+    def set_output_reaper(self, data):
+        self.output_reaper = data
+
+    def get_output_trackslive(self):
+        return self.output_trackslive
+
+    def set_output_trackslive(self, data):
+        self.output_trackslive = data
+
+    def get_output_write_to_console(self):
+        return self.output_write_to_console
+
+    def set_output_write_to_console(self, data):
+        self.output_write_to_console = data
+
+    def get_output_write_to_csv(self):
+        return self.output_write_csv
+
+    def set_output_write_to_csv(self, data):
+        self.output_write_csv = data
--- src/Toolinfo.py
+++ src/Toolinfo.py
@@ -6,5 +6,5 @@
 #
 ####################################################
 tool_name = "Channel List Manager for Allen & Heath dLive and Avantis"
-version = "2.12.0-RC8"
+version = "2.12.0-RC9"
 build_date = "May 2026"
